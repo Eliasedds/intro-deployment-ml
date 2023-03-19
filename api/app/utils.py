47bbbb3b239c311d6672d887f640757fa95d890d
elias <eliasdudamel@gmail.com>
2023-03-18 22:20:22 -0300

    Adding testing

--- api/app/utils.py
+++ api/app/utils.py
@@ -13,5 +13,5 @@ def get_model() -> Pipeline:
 
 def transform_to_dataframe(class_model: BaseModel) -> DataFrame:
     transition_dictionary = {key:[value] for key, value in class_model.dict().items()}
-    data_frame = transition_dictionary
+    data_frame = DataFrame(transition_dictionary)
     return data_frame
